6c17e3d13ffac76b7d3790f4d193446b12915b3d
Divyanshu Dhiman <divyanshudhiman51@gmail.com>
2026-04-07 17:43:38 +0530

    ..

--- api/chat.js
+++ api/chat.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
 
     const model = genAI.getGenerativeModel({
       model: "gemini-2.5-flash", // ✅ correct
+      "gemini-1.5-flash"
     });
 
     const result = await model.generateContent(message);
